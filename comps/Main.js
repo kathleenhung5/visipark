f942f6d8f6eecc6f2273f86d82a0941a914e530f
eliastayag <misterghost999@gmail.com>
2019-10-17 15:22:56 -0700

    Foundation files set up...

---
+++ comps/Main.js
@@ -0,0 +1,16 @@
+  
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from '../styles/MainStyles';
+
+
+function Main(){
+    // UI
+    return (
+        <View style={styles.container}>
+            <Text>VisiPark!!</Text>
+        </View>
+    )
+}
+
+export default Main;
---
+++ styles/MainStyles.js
@@ -0,0 +1,13 @@
+
+import {StyleSheet} from 'react-native';
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%"
+    }
+});
+
+export default styles;
