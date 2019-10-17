88a4db7d4126686b29829558c25eb9f0e356ba8f
eliastayag <misterghost999@gmail.com>
2019-10-17 14:39:07 -0700

    added react

---
+++ ios/Pods/Target Support Files/Pods-visipark-tvOS/Pods-visipark-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_visipark_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_visipark_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-visiparkTests/Pods-visiparkTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_visiparkTests : NSObject
+@end
+@implementation PodsDummy_Pods_visiparkTests
+@end
