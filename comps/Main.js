b4f292d257b91b98f32862d5a0567086f1d026f8
kathleenhung5 <kathleenhung5@gmail.com>
2019-10-28 22:07:43 -0700

    started building tab bar in <Tenant />
    the {Content} part is working when clicking on different tabs. I still need to import font family for styling.

--- comps/Main.js
+++ comps/Main.js
@@ -2,13 +2,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import styles from '../styles/MainStyles';
-
+import Tenant from './Tenant/Tenant';
 
 function Main(){
     // UI
     return (
         <View style={styles.container}>
-            <Text>VisiPark!!</Text>
+            <Tenant />
         </View>
     )
 }
--- comps/Tenant/Tenant.js
+++ comps/Tenant/Tenant.js
@@ -1,13 +1,87 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React,{useState} from 'react';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+import Visitors from './Visitors';
+import History from './History';
+import Report from './Report';
+import styles from '../../styles/TenantStyles';
 
 
 function Tenant(){
+  const [cont, setCont] = useState('Visitors');
+
+// img src variables
+  var visitorimg = null;
+  var historyimg = null;
+  var reportimg = null;
+
+// Content is everything above tab bar
+  var Content = null;
+
+// conditions updating Content and tabbar
+  if(cont == 'Visitors'){
+    Content = <Visitors />
+    visitorimg = 'ppl-purp.png';
+    historyimg = 'history-grey.png';
+    reportimg = 'report-grey.png';
+  }
+  if(cont =='History'){
+    Content = <History />
+    visitorimg = 'ppl-grey.png';
+    historyimg = 'history-purp.png';
+    reportimg = 'report-grey.png';
+  }
+  if(cont == 'Report'){
+    Content = <Report />
+    visitorimg = 'ppl-grey.png';
+    historyimg = 'history-grey.png';
+    reportimg = 'report-purp.png';
+  }
 
 
   return(
-    <View>
+    <View style={styles.container}>
+      {Content}
+
+      {/* Tab bar starts here */}
+      <View style={styles.tabbar}>
+
+          <TouchableOpacity 
+            style = {styles.tabcont}
+            onPress={()=> setCont('Visitors')}>
+            <Image 
+              style = {styles.tabimg}
+              source = {require("../../img/ppl-purp.png")}
+              resizeMode = 'contain'
+            />
+            <Text style={styles.tabtext}>Visitors</Text>
+          </TouchableOpacity>
+          
+          <TouchableOpacity 
+            style = {styles.tabcont}
+            onPress={()=> setCont('History')}>
+            <Image 
+              style = {styles.tabimg}
+              source = {require("../../img/history-grey.png")}
+              resizeMode = 'contain'
+            />
+            <Text style={styles.tabtext}>History</Text>
+          </TouchableOpacity>
+          
+          <TouchableOpacity 
+            style = {styles.tabcont}
+            onPress={()=> setCont('Report')}>
+            <Image 
+              style = {styles.tabimg}
+              source = {require("../../img/report-grey.png")}
+              resizeMode = 'contain'
+            />
+            <Text style={styles.tabtext}>Report</Text>
+          </TouchableOpacity>
+
+      </View>
+      {/* End of Tab bar */}
     </View>
+      
   )
 }
 
