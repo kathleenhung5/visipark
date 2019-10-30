7c6d85d92cf9b3e7e04df587042ca332cb974b99
Nicole <nicole.ll.930529@gmail.com>
2019-10-29 23:44:02 -0700

    more fonts

--- comps/Login.js
+++ comps/Login.js
@@ -1,41 +1,54 @@
 import React, {useState} from 'react';
 import {View, Text, Image, TouchableOpacity, TextInput} from 'react-native';
 import styles from '../styles/MainStyles';
+import Tform from './Tform';
+import BMform from './BMform';
 
 
 
 function Login(){
   const [showTenantPage, setShowTenantPage] = useState(false);
+  const [showBMPage, setShowBMPage] = useState(false);
 
   var TenantPage = null;
   if (showTenantPage === true){
   TenantPage=(
-
-      <View style={styles.container}>
-        <Text style={styles.LogoText}>VisiPark</Text>
-        <TextInput
-          style={{ height: 40, borderColor: 'white', borderWidth: 1, borderRadius:25 }}
-          />
-        <TextInput
-            style={{ height: 40, borderColor: 'white', borderWidth: 1, borderRadius:25 }}
-            />
-      </View>
+      <Tform />
   );
+  }
+  var BMPage = null;
+  if (showBMPage === true){
+    BMPage=(<BMform/>);
   }
   return(
     <View style={styles.container}>
     {TenantPage}
+    {BMPage}
+      <Image source={require('../img/Logo.png')}
+      style={styles.Logo}
+      />
       <Text style={styles.LogoText}>VisiPark</Text>
-      <Text style={styles.LoginText}>Login as</Text>
+      <Text style={styles.LoginText}>
+      {showTenantPage ? "apartment unit" : "Login as"}
+      </Text>
 
       <View>
-        <TouchableOpacity style={styles.BMBtn}>
-          <Text style={styles.BMText}>Building Manager</Text>
+        <TouchableOpacity
+        style={[styles.BMBtn,{display:showTenantPage ? "none" : ""},
+          {backgroundColor:showBMPage ? "white" : "#863AE8"}]}
+        onPress={()=>{setShowBMPage(!showBMPage)}}
+        >
+          <Text
+          style={[styles.BMText,{color:showBMPage ? "#863AE8" : "white"}]}>
+          {showBMPage ? "login as manager" : "Building Manager"}
+          </Text>
          </TouchableOpacity>
 
-        <TouchableOpacity style={styles.TenantBtn}
+        <TouchableOpacity style={[styles.TenantBtn,{display:showBMPage ? "none" : ""}]}
         onPress={()=>{setShowTenantPage(!showTenantPage)}}>
-          <Text style={styles.TenantText}>Tenants</Text>
+          <Text style={styles.TenantText}>
+          {showTenantPage ? "login as tenant" : "Tenant"}
+          </Text>
         </TouchableOpacity>
       </View>
     </View>
