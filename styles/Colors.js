03dae967a38ad264cf7ec1a92b8d0e85d81d6145
kathleenhung5 <kathleenhung5@gmail.com>
2019-10-30 16:32:40 -0700

    Completed setting up Colors.js

--- styles/Colors.js
+++ styles/Colors.js
@@ -8,13 +8,16 @@ export const Colors = {
   Glack: '#151515'
 };
 
+
 // How to use global colour
 // In your component.js
-// import Colors from '../styles/Colors';
+
+
+// import {Colors} from '../styles/Colors';
 
 //var styles = Stylesheet.create({
 //      text:{
 //      color: Colors.Purple
 //        }
-// })
+// });
 
