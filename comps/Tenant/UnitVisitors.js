a55698f3461c3f43cea804fba0c1346d1960ae82
eliastayag <misterghost999@gmail.com>
2019-10-30 20:24:25 -0700

    unfished visitors page...

--- comps/Tenant/UnitVisitors.js
+++ comps/Tenant/UnitVisitors.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
 import {Colors} from '../../styles/Colors';
 import Texts from '../../styles/Texts';
 import styles from '../../styles/UnitVisitors'
 
 
 
+
 function UnitVisitors(){
     return(
+        <TouchableOpacity>
     <View style={styles.Box}>
+        <Image source={require('../../img/add-visi.png')} style={styles.Img}/>
         <Text>Add Visitor</Text>
-            <View style={styles.Box1}>
+
+
     </View>
-    </View>
+    </TouchableOpacity>
     )
 }
 
--- styles/UnitVisitors.js
+++ styles/UnitVisitors.js
@@ -3,24 +3,34 @@ import {StyleSheet} from 'react-native';
 
 var styles = StyleSheet.create({
     Box:{
-
+        alignItems: "center",
+        justifyContent: "center",
         width: 375,
         height: 200,
-        borderRadius: 20,
-        backgroundColor: "#bcbcbc"
+        borderRadius: 30,
+        backgroundColor: "#fafafa",
+        borderStyle: "dashed",
+        borderColor: "#863ae8",
+        borderWidth: 5.5,
+
+
+
         
 
 
     },
     Box1:{
-
+        alignItems: "center",
+        justifyContent: "center",
         width: 375,
         height: 200,
         borderRadius: 20,
         backgroundColor: "#bcbcbc"
+    },
+    Img:{
+        width: "30%",
+        height: "50%",
         
-
-
     }
 })
 
