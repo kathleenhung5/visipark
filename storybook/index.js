d5129a243b0bb6868e587b8328e31a73f76e8c37
kathleenhung5 <kathleenhung5@gmail.com>
2019-11-08 11:35:49 -0800

    Storybook files working now :D

--- storybook/index.js
+++ storybook/index.js
@@ -1,7 +1,7 @@
 import { AppRegistry } from 'react-native';
 import { getStorybookUI, configure } from '@storybook/react-native';
 
-import './rn-addons';
+//import './rn-addons';
 
 // import stories
 configure(() => {
@@ -12,8 +12,9 @@ configure(() => {
 // To find allowed options for getStorybookUI
 const StorybookUIRoot = getStorybookUI({});
 
-// If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
+// If you are using React Native vanilla write your app name here.
 // If you use Expo you can safely remove this line.
 //AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot);
 
 export default StorybookUIRoot;
+
