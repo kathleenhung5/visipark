d817927ba7e5ccb568898c379a5bb04ed7ae1972
eliastayag <misterghost999@gmail.com>
2019-11-08 12:38:06 -0800

    Building Manager Set up
    Building manager login button works, building manager tab bar made and functional

--- styles/PagesStyles/ManagerStyles.js
+++ styles/PagesStyles/ManagerStyles.js
@@ -4,8 +4,39 @@ import Texts from '../Texts'
 
 var styles = StyleSheet.create({
     container:{
-        
+        flex:1,
+        width:"100%",
     },
+    tabbar:{
+        position:'absolute',
+        bottom: 0,
+        left: 0,
+        height: 70,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent:'space-around',
+        alignItems:'center',
+        backgroundColor: 'white',
+        // drop shadow
+        shadowColor: Colors.Purple,
+        shadowOffset: {width: 0, height: 0},
+        shadowOpacity: 0.13,
+        shadowRadius: 13
+    },
+    tabcont:{
+        flex:1,
+        flexDirection: 'column',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    tabimg:{
+        width: 32,
+        height: 40
+    },
+    tabtext:{
+        fontFamily:'OpenSans-Bold',
+        fontSize: 10
+    }
     
 });
 
