5995ab643fb3efb6f628b011cb47fa0bfa22c5c0
kathleenhung5 <kathleenhung5@gmail.com>
2019-11-10 15:13:02 -0800

    Set up and tested php file. Added api in Main.js.
    Get seems to be working now. The app gets all the tables in the database when it loads.
    Post is still in progress.

--- Pages/Main.js
+++ Pages/Main.js
@@ -1,16 +1,58 @@
 
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import {View, Text} from 'react-native';
 import styles from '../styles/PagesStyles/MainStyles';
 import Tenant from '../Pages/Tenant';
 import Login from '../Pages/Login';
 import Popup from '../comps/Popup';
 import Manager from '../Pages/Manager';
+import axios from 'axios';
+import Axios from 'axios';
 
 
 
 
 function Main(props){
+// --- Communicate with DB ----
+    const [dbUnits, setDbUnits] = useState([]);
+    const [dbVisitors, setDbVisitors] = useState([]);
+    const [dbReports, setDbReports] = useState([]);
+    
+    // getData Function
+    var getData = async()=>{
+        var resp = await fetch('http://localhost:8888/visipark/visipark.php');
+        var data = await resp.json();
+        setDbUnits(data.data.units);
+        setDbVisitors(data.data.visitors);
+        setDbReports(data.data.reports);
+        console.log('units',dbUnits);
+        console.log('visitors',dbVisitors);
+        console.log('reports',dbReports);
+    }
+
+    // postData Function
+    const addVisitor = async()=>{
+        var visitor = {
+            table:"visitors",
+            data: {
+                unit_num: 101,
+                name: "Visitor Name",
+                plate: "vis plt"
+            }
+        }
+        var data = await axios.post("http://localhost:8888/visipark/visipark.php", visitor);
+        console.log("create", data);
+        }
+    
+
+
+    // ---- Run getData when it loads
+    useEffect(()=>{
+        addVisitor();
+       getData();
+    },[]);
+
+// ---- Pop up ---
     // Function for Popup
     // Call showPop('YourPopupTitle') in your button to show the corresponding Popup.
     // Example: Your Popup title is 'Add Visitor', call showPop('AddVisitor') in your onPress.
